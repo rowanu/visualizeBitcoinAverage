8662b5070b2fb1bd110074a4433e3ddb5dae8360
rowan <rowanu@gmail.com>
2013-12-26 19:07:19 -0300

    Updates

---
+++ js/treemap.js
@@ -0,0 +1,63 @@
+/*jslint browser: true */
+/*globals d3 */
+var margin = {top: 40, right: 10, bottom: 10, left: 10},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+var color = d3.scale.category20();
+
+var treemap = d3.layout.treemap()
+  .size([width, height])
+  .sort(function (a, b) { return a.volume - b.volume; })
+  .value(function(d) { return d.volume; });
+
+var div = d3.select('body').append('div')
+  .style('position', 'relative')
+  .style('width', (width + margin.left + margin.right) + 'px')
+  .style('height', (height + margin.top + margin.bottom) + 'px')
+  .style('left', margin.left + 'px')
+  .style('top', margin.top + 'px');
+
+// Gets the selected currency from URL params, or defalut to 'USD'
+function getSelectedCurrency(path) {
+  var parts = path.split('#!');
+  var selected = (parts.length > 1) ? parts[1] : 'USD';
+  return selected.replace('/', '');
+}
+
+function volumesObjectToArray(exchanges) {
+  var exchange, volumes = {name: 'volumes'};
+  delete exchanges.timestamp;
+  volumes.children = [];
+  for (exchange in exchanges) {
+    if (exchanges.hasOwnProperty(exchange)) {
+      volumes.children.push({
+        name: exchange,
+        volume: exchanges[exchange].volume_btc * 100
+      });
+    }
+  }
+  return volumes;
+}
+
+function position() {
+  this.style("left", function(d) { return d.x + "px"; })
+      .style("top", function(d) { return d.y + "px"; })
+      .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+      .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+}
+
+var url = 'https://api.bitcoinaverage.com/exchanges/' + getSelectedCurrency(window.location.href);
+d3.json(url, function (err, exchanges) {
+  if (err) { console.error(err); }
+  // var timestamp = exchanges.timestamp;
+  var volumes = volumesObjectToArray(exchanges);
+
+  var node = div.datum(volumes).selectAll(".node")
+    .data(treemap.nodes)
+  .enter().append("div")
+    .attr("class", "node")
+    .call(position)
+    .style("background", function (d) { return color(d.volume); })
+    .text(function(d) { return d.name; });
+});
